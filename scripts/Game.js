85ab542ffba6720ba6f1b02d718a036b10359bc0
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2023-10-27 21:47:10 +0800

    Test switching Levels

--- scripts/Game.js
+++ scripts/Game.js
@@ -1,10 +1,13 @@
 
+
 class Level1 extends Phaser.Scene {
   constructor() {
-    super({ key: "Level1", active: true });
+  //   super({ key: "Level1", active: true });
+    super('Level1')
   }
 
   preload() {
+    this.load.image('Bg', 'assets/Environment/Levels/Bg.png');
     this.load.image("player", "assets/Player/player.png", {
       frameWidth: 171,
       frameHeight: 144,
@@ -14,6 +17,8 @@ class Level1 extends Phaser.Scene {
   create() {
     const PlayerPositionY = 10;
     const PlayerPositionX = 50;
+
+    this.add.image(960, 135, 'Bg')
     
     Level1.player = this.physics.add.sprite(
       PlayerPositionX,
@@ -23,10 +28,71 @@ class Level1 extends Phaser.Scene {
 
     Level1.player.setBounce(0.2);
     Level1.player.setCollideWorldBounds(true);
+    this.input.manager.enabled = true;
+
+    this.input.once('pointerdown', function () {
+
+      this.scene.start('Level2');
+    
+    }, this);
+    
   }
 
-  update() {}
+  update() {
+    // const cam = this.cameras.main;
+    // const speed = 3;
+    // if(cursors.left.isDown){
+    //     // move left
+    //     cam.scrollX -= speed 
+    // }
+  }
+
+
 }
+
+class Level2 extends Phaser.Scene {
+    constructor() {
+      // super({ key: "Level2", active: false });
+      super('Level2')
+    }
+  
+    preload() {
+      this.load.image("player", "assets/Player/player.png", {
+        frameWidth: 171,
+        frameHeight: 144,
+      });
+    }
+  
+    create() {
+      const PlayerPositionY = 20;
+      const PlayerPositionX = 200;
+      
+      Level2.player = this.physics.add.sprite(
+        PlayerPositionX,
+        PlayerPositionY,
+        "player"
+      );
+  
+      Level2.player.setBounce(0.2);
+      Level2.player.setCollideWorldBounds(true);
+
+      this.input.manager.enabled = true;
+
+      this.input.once('pointerdown', function () {
+
+        this.scene.start('Level1');
+
+    }, this);
+    
+    }
+  
+    update() {}
+  }
+
+
+
+
+
 var config = {
   type: Phaser.AUTO,
   width: 800,
@@ -39,7 +105,7 @@ var config = {
       debug: false,
     },
   },
-  scene: [Level1],
+  scene: [Level1, Level2],
 };
 
 var game = new Phaser.Game(config);
