f3a3672a1411747056518a6a18f006ac336b4d66
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2023-10-27 20:15:08 +0800

    Made Level1 class work for the scene- WIP

--- scripts/Game.js
+++ scripts/Game.js
@@ -1,44 +1,45 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    backgroundColor: '#4488aa',
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity:{y:300},
-            debug: false
-        }
-    },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+
+class Level1 extends Phaser.Scene {
+  constructor() {
+    super({ key: "Level1", active: true });
+  }
+
+  preload() {
+    this.load.image("player", "assets/Player/player.png", {
+      frameWidth: 171,
+      frameHeight: 144,
+    });
+  }
+
+  create() {
+    const PlayerPositionY = 10;
+    const PlayerPositionX = 50;
+    
+    Level1.player = this.physics.add.sprite(
+      PlayerPositionX,
+      PlayerPositionY,
+      "player"
+    );
+
+    Level1.player.setBounce(0.2);
+    Level1.player.setCollideWorldBounds(true);
+  }
+
+  update() {}
 }
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: "#4488aa",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
+  scene: [Level1],
+};
 
 var game = new Phaser.Game(config);
-
-
-const PlayerPositionY = 10;
-const PlayerPositionX = 50;
-
-
-function preload ()
-{
-    this.load.image('player', 'assets/Player/player.png', {frameWidth:171, frameHeight: 144});
-}
-
-function create ()
-{
- 
-    player = this.physics.add.sprite(PlayerPositionX, PlayerPositionY, 'player');
-    
-    player.setBounce(0.2);
-    player.setCollideWorldBounds(true);
-
-}
-
-function update ()
-{
-}
