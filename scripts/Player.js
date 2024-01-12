0f190f19b12ae4d8e0d3815d846daf74a1bd4dd0
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2024-01-12 18:32:27 +0800

    Fix moonwalk issue with player when flipping

--- scripts/Player.js
+++ scripts/Player.js
@@ -41,15 +41,22 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     }
     // WALK Movement
     if (inputController.keyShift.isDown && inputController.keyA.isDown) {
+      this.setFlipX(true);
+
       this.setVelocityX(-160); // Move Left
-      this.anims.play("runLeft", true);
+      // this.anims.play("runLeft", true);
+      this.anims.play("runRight", true);
     } else if (cursors.left.isDown || inputController.keyA.isDown) {
+      this.setFlipX(true);
       this.setVelocityX(-190); // Move Left
-      this.anims.play("left", true);
+      // this.anims.play("left", true);
+      this.anims.play("right", true);
     } else if (inputController.keyShift.isDown && inputController.keyD.isDown) {
+      this.setFlipX(false);
       this.setVelocityX(190); // Move right
       this.anims.play("runRight", true);
     } else if (cursors.right.isDown || inputController.keyD.isDown) {
+      this.setFlipX(false);
       this.setVelocityX(160); // Move right
       this.anims.play("right", true);
     } else {
