1a3ecbb930706df9479241541b64883826141955
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2024-01-19 18:54:10 +0800

    Fix issue with level 2 not loading

--- scripts/Level2.js
+++ scripts/Level2.js
@@ -15,7 +15,19 @@ export default class Level2 extends BaseScene {
         this.building();
         super.create();
 
-       
+        let particleOptions = {
+          frame: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+          angle: { min: 180, max: 360 },
+          speed: { min: 50, max: 200 },
+          frequency: 50,
+          gravityY: 200,
+          scale: { start: 1, end: 1 },
+          alpha: { start: 1, end: 0 },
+          lifespan: { min: 500, max: 2500 },
+          blendMode: "ADD", // lighten
+        };
+        let particles = this.add.particles(10, 550, "particles", particleOptions);
+        particles.setDepth(999);
     }
 
 
