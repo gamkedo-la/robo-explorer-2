d33a778d4bd2fae1647619a4e109ba3b3062916c
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2024-02-18 22:25:39 +0800

    adjust building with van

--- scripts/Level1.js
+++ scripts/Level1.js
@@ -10,10 +10,10 @@ export default class Level1 extends BaseScene {
   
     create()
     {
-      this.cameras.main.setBounds(0, 0, 4000, 600);
+      this.cameras.main.setBounds(0, 0, 5000, 600);
       this.physics.world.bounds.width = 2000;
       this.physics.world.bounds.height = 600;
-        this.foreground = this.add.tileSprite(0, 38, 800, 296, 'foreground').setOrigin(0, 0);
+        // this.foreground = this.add.tileSprite(0, 38, 800, 296, 'foreground').setOrigin(0, 0);
        
         // Set up collision between player and killerBee
         
