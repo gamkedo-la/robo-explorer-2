a84f4814ab2a239b38484bd79bd40accca13e1a4
Rodrigo Bonzerr S. Lopez <rodprogramdev@gmail.com>
2024-02-26 22:06:31 +0800

    test adding subway art tiles on level1

--- scripts/Level1.js
+++ scripts/Level1.js
@@ -17,7 +17,7 @@ export default class Level1 extends BaseScene {
         [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
         [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
         [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-        [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [ 0, 0, 0, 0, 0, 9, 10, 11, 0, 0, 0 ],
         [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
         [ 0, 1, 2, 0, 0, 0, 0, 1, 2, 0, 0 ],
         [ 0, 5, 6, 0, 7, 7, 0, 5, 6, 7, 7 ],
@@ -28,7 +28,7 @@ export default class Level1 extends BaseScene {
     const map = this.make.tilemap({ data: level, tileWidth: 64, tileHeight: 64 });
     const tiles = map.addTilesetImage('tileArt');
     const layer = map.createLayer(0, tiles, 0, 0);
-    map.setCollision([ 1, 2,3,4,5,6,7 ]);
+    map.setCollision([ 1, 2,3,4,5,6,7,8,9,10,11 ]);
     
 
     this.cameras.main.setBounds(0, 0, 4000, 600);
