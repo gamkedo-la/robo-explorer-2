5156e9e36629b4cd79edaf283b2046bd5026dad8
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2024-03-10 15:36:39 -0500

    rendering second sprite on top of level selection frame

--- scripts/UI/BossFrame.js
+++ scripts/UI/BossFrame.js
@@ -1,9 +1,10 @@
 export default class BossFrame extends Phaser.Physics.Arcade.Sprite {
   sceneRef;
 
-  constructor(scene, x, y, texture) {
+  constructor(scene, x, y, texture, overlaySprite) {
     super(scene, x, y, texture);
     this.sceneRef = scene;
     this.sceneRef.add.existing(this);
+    this.sceneRef.add.image(x, y, overlaySprite);
   }
 }
